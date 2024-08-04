3e8708bbabf16da101405ba717ec5fa7181b4ec9
raydasher9 <nickopie3@gmail.com>
2024-08-04 11:57:39 -0400

    a

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const playVideo = document.getElementById("art")
+
+ const x = function () {
+        playvideo.play()
+     }
+
+const y = function () {
+        playvideo.pause()
+     }
